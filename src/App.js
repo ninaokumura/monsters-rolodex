5724e5903797963b738093f6f39339633951b10f
Nina Okumura <elaineokumura@gmail.com>
2021-12-02 16:18:37 +1300

    Implent reusable search box component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -25,11 +27,11 @@ class App extends Component {
     );
     return (
       <div className='App'>
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monsters'
-          onChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={e => this.setState({ searchField: e.target.value })}
         />
+
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
